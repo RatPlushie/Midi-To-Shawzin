faf8ec9796b0bf4bf68e5e715fdb4b1a926ebc1b
aki <kieran.mortimer@gmail.com>
2021-07-05 12:13:45 +0200

    console asks user for correct usb device

--- connection.py
+++ connection.py
@@ -3,25 +3,35 @@ import serial.tools.list_ports
 import usb
 
 def list_usb_devices():
-	devlist = usb.core.find(find_all=True)
+	devlist = list(usb.core.find(find_all=True))
 
 	count = 1
 	for dev in devlist:
 		dev_string = str(count) + ") " + str(dev.manufacturer) + " " + str(dev.product)
 		print(dev_string)
 		count += 1
+	
+	print()
+	midi_input = input('Select Midi device: ')
 
+	print(str(devlist[int(midi_input) - 1].manufacturer), str(devlist[int(midi_input) - 1].product) + ' selected')
 
+	return devlist[int(midi_input) - 1]
+	
+
+'''
 def get_serial_port():
 	# TODO Using the selected serial port connecting to the device instance.
 	pass
-
+'''
 
 def connect_to_midi():
+	print()
 	print('Discovering USB devices...')
 
-	list_usb_devices()
-	
+	# Displaying and asking the user for the correct USB Midi device
+	usb_dev = list_usb_devices()
+
 	#connection = MidiConnector('')
 
 	
